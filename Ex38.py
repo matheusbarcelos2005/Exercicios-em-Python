a813231839e73008ba535b6c806507519f64a06b
Matheus Barcelos Alves <matheusbarcelosbr@gmail.com>
2025-06-09 20:48:34 -0300

    Create Ex38.py

---
+++ Ex38.py
@@ -0,0 +1,8 @@
+Num1 = int(input('Digite o primeiro número: '))
+Num2 = int(input('Digite o segundo número: '))
+if Num1 > Num2:
+    print('O primeiro número é maior que o segundo.')
+elif Num1 < Num2:
+    print('O segundo número é maior que o primeiro.')
+else:
+    print('Os números são iguais.')
