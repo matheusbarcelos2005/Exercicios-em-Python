46521bcf4de0fb93ffb196153a1a3156389b3b71
Matheus Barcelos Alves <matheusbarcelosbr@gmail.com>
2025-06-09 22:21:54 -0300

    Create Ex44.py

---
+++ Ex44.py
@@ -0,0 +1,23 @@
+print('{:=^30}'.format('Teste de venda'))
+
+preço = float(input('Preço das compras: R$ '))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista no cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R$ {:.2f} SEM JUROS'.format(parcela))
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    parcelas = int(input('Quantas parcelas? '))
+    parcela = total / parcelas
+    print('Sua compra será parcelada em {}x de R$ {:.2f} COM JUROS'.format(parcelas, parcela))
