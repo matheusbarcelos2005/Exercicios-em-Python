5ed7a8316f76cb9f3fdef2b25fd7f63e594f1998
Matheus Barcelos Alves <matheusbarcelosbr@gmail.com>
2025-06-11 21:07:56 -0300

    add

--- Ex50.py
+++ Ex50.py
@@ -3,4 +3,4 @@ for Pergunta in range (1,7):
     PerNum = int(input("Digite um número: "))
     if PerNum % 2 == 0:
         soma += PerNum
-print('A soma dos números pares é: {}'.format(soma))
+print('A soma dos números pares é: {}'.format(soma))    
