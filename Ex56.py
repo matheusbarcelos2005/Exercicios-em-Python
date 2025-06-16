f3967559bdc1d6afa9106a1cdd7d1ae099ce76e8
Matheus Barcelos Alves <matheusbarcelosbr@gmail.com>
2025-06-16 17:18:50 -0300

    Create Ex56.py

---
+++ Ex56.py
@@ -0,0 +1,22 @@
+totmulher = 0
+maioridade = 0
+menoridade = 0
+for p in range (1,5):
+    print('----- {}ª pessoa -----'.format(p))
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip().upper()
+    media = idade / 4
+    if idade == 1:
+        maioridade = idade
+        menoridade = idade
+    else:
+        if idade > maioridade:
+            maioridade = idade
+        if idade < menoridade:
+            menoridade = idade
+    if sexo == 'F' and 'f' and idade < 20:
+        totmulher += 1
+print('A média de idade do grupo é de {} anos'.format(media))
+print('O homem mais velho tem {} anos e se chama {}'.format(maioridade, nome))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totmulher))
