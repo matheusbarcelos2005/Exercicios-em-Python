c34486eef4c000f2fc8d5b81fec732fb6621058f
Matheus Barcelos Alves <matheusbarcelosbr@gmail.com>
2025-06-16 17:32:48 -0300

    Update Ex56.py

--- Ex56.py
+++ Ex56.py
@@ -1,12 +1,15 @@
 totmulher = 0
 maioridade = 0
 menoridade = 0
+somaidade = 0
+mediaidade = 0
 for p in range (1,5):
     print('----- {}ª pessoa -----'.format(p))
     nome = str(input('Nome: '))
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).strip().upper()
-    media = idade / 4
+    somaidade += idade
+    mediaidade = somaidade / 4
     if idade == 1:
         maioridade = idade
         menoridade = idade
@@ -17,6 +20,6 @@ for p in range (1,5):
             menoridade = idade
     if sexo == 'F' and 'f' and idade < 20:
         totmulher += 1
-print('A média de idade do grupo é de {} anos'.format(media))
+print('A média de idade do grupo é de {} anos'.format(mediaidade))
 print('O homem mais velho tem {} anos e se chama {}'.format(maioridade, nome))
 print('Ao todo são {} mulheres com menos de 20 anos'.format(totmulher))
